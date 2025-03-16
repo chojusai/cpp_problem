c45e2833b5a8c7bd7404a9b41489dbdf4020cc82
chojusai <chojusai@naver.com>
2025-03-16 16:40:01 +0900

    수정

--- chapter_2/2_3.cpp
+++ chapter_2/2_3.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
-#include<cstring>
 using namespace std;
-
 int main() {
-	cout << "두 수를 입력하라>> ";
-	char text[100];
-	cin.getline(text, 100, '\n');
-	for (int i = 0; i < strlen(text); i++) {
-		for (int j = 0; j < i + 1; j++) {
-			cout << text[j];
-		}
-		cout << '\n';
+	int a, b;
+	cout << "두 수를 입력하라>>";
+	cin >> a>>b;
+	int result = 0;
+	if (a > b) {
+		result = a;
 	}
+	else if (a < b) {
+		result = b;
+	}
+	else {
+		result = a;
+	}
+	cout << "큰 수" << '=' << result;
+
 }
